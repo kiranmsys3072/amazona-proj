f0b0f92d9fc449e2d6d7488ef5e7d3277e28cd8a
Kelangi Raj Kiran <kkiran@msystechnologies.com>
2023-08-16 12:28:42 +0530

    01-initialized React app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div >
+      <header>
+       <a href='/'>Amazona</a>
+      </header>
+      <main>
+        <h1>List Products</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
