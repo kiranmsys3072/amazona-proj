ce6ef0a7b6b62d096208342b7587a1fa3d492cda
Kelangi Raj Kiran <kkiran@msystechnologies.com>
2023-08-16 14:51:46 +0530

    03-prepare basic UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from './data'
 
 
 function App() {
@@ -7,7 +8,26 @@ function App() {
        <a href='/'>Amazona</a>
       </header>
       <main>
-        <h1>List Products</h1>
+       <h1>Featured Products</h1>
+       <div className="products">
+       {
+        data.products.map((product)=>{
+          return <div key={product.slug} className="product">
+            <a href ={`/products/${product.slug}`}   >
+            <img src={product.image} alt={product.name}/>
+            </a>
+            <div className="product-info">
+            <a href ={`/products/${product.slug}`}   >
+            <p>{product.name}</p>
+            </a>
+            <p> <strong>${product.price}</strong></p>
+            </div>
+            
+          </div>
+        })
+       }
+       </div>
+      
       </main>
     </div>
   );
